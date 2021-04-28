3680fc27b745c573210873bf1e29350526ca974d
1797878411@qq.com <1797878411@qq.com>
2021-04-28 21:22:01 +0800

    新增find查找函数，并且有实例

--- C++第三阶段/5.1常用算法.cpp
+++ C++第三阶段/5.1常用算法.cpp
@@ -29,6 +29,33 @@ public:
 	}
 };
 
+class findperson
+{
+public:
+	findperson()
+	{
+
+	}
+	findperson(string name,int age)
+	{
+		this->name = name;
+		this->age = age;
+	}
+
+	bool operator==(const findperson& p)
+	{
+		if (p.name == this->name && p.age == this->age)
+		{
+			return true;
+		}
+		return false;
+	}
+
+public:
+	string name;
+	int age;
+};
+
 
 void com_use_algorithm(void)
 {
@@ -69,9 +96,39 @@ void com_use_algorithm(void)
 	//查找指定元素，找到返回指定元素的迭代器，找不到返回结束迭代器end()
 	//函数原型-find(iterator beg,iterator end,value);
 	// beg 开始迭代器   end 结束迭代器   value 查找的元素
+	vector<int>::iterator it = find(v1.begin(), v1.end(), 5);
+	if (it == v1.end())
+	{
+		cout << "查找元素算法find（）没有找到该元素" << endl;
+	}
+	else
+	{
+		cout << "查找元素算法find（）找到该元素 : " << *it << endl;
+	}
 
+	vector<findperson>v3;
 
+	findperson fp1("aaa", 374);
+	findperson fp2("bbb", 364);
+	findperson fp3("ccc", 687);
+	findperson fp4("ddd", 785);
+	findperson fp5("fff", 432);
 
+	v3.push_back(fp1);
+	v3.push_back(fp2);
+	v3.push_back(fp3);
+	v3.push_back(fp4);
+	v3.push_back(fp5);
+
+	vector<findperson>::iterator fpit = find(v3.begin(), v3.end(), fp2);
+	if (fpit == v3.end())
+	{
+		cout << "查找元素算法find（）没有找到该元素" << endl;
+	}
+	else
+	{
+		cout << "查找元素算法find（）找到该元素--姓名：" << fpit->name << " 年龄：" << fpit->age << endl;
+	}
 
 
 }
